4bc123027610ebc41db5847365ac497911bccc8f
Keivn <xiong-wei.dai@hp.com>
2022-01-20 18:13:47 +0800

    Init

--- app.go
+++ app.go
@@ -11,22 +11,21 @@ import (
 
 type App struct {
 	Router *mux.Router
-	DB *sql.DB
+	DB     *sql.DB
 }
 
-func (a *App) Initialize(user, password, dbname string)  {
-	connectionString := fmt.Sprintf("%s:%s@/%s",user, password, dbname)
-
+func (a *App) Initialize(user, password, dbname string) {
+	connectionString := fmt.Sprintf("%s:%s@/%s", user, password, dbname)
 
 	var err error
-	a.DB, err = sql.Open("mysql",connectionString)
-	if(err != nil) {
+	a.DB, err = sql.Open("mysql", connectionString)
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	a.Router = mux.NewRouter()
 }
 
-func (a *App) Run(addr string)  {
+func (a *App) Run(addr string) {
 
 }
--- model.go
+++ model.go
@@ -3,32 +3,67 @@ package main
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 )
 
 type user struct {
-	ID int `json:"id"`
+	ID   int    `json:"id"`
 	Name string `json:"name"`
-	Age int `json:"age"`
+	Age  int    `json:"age"`
 }
 
-func (u *user) getUser(db *sql.DB) error  {
+func (u *user) getUser(db *sql.DB) error {
+	statement := fmt.Sprintf("SELECT name ,age FROM users WHERE id=%d, u.ID")
+	return db.QueryRow(statement).Scan(&u.Name, &u.Age)
+}
+
+func (u *user) deleteUser(db *sql.DB) error {
+	statement := fmt.Sprintf("DELETE FROM users WHERE id=%d", u.ID)
+	_, err := db.Exec(statement)
+	return err
+}
+
+func (u *user) updateUser(db *sql.DB) error {
 	return errors.New("Not implemented")
-}
-
-func (u *user) deleteUser(db *sql.DB) error  {
-	return errors.New("Not implemented")
 
 }
 
-func (u *user) updateUser(db *sql.DB) error  {
-	return errors.New("Not implemented")
+func (u *user) createUser(db *sql.DB) error {
+	statement := fmt.Sprintf("INSERT INTO users(name, age) VALUES ('%s', %d)", u.Name, u.Age)
+	_, err := db.Exec(statement)
 
+	if err != nil {
+		return err
+	}
+
+	err = db.QueryRow("SELECT LAST_INSERT_ID()").Scan(&u.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (u *user) createUser(db *sql.DB) error  {
-	return errors.New("Not implemented")
-}
+func getUsers(db *sql.DB, start, count int) ([]user, error) {
+	statement := fmt.Sprintf("SELECT id, name, age FROM users LIMIT %d OFFSET %d", count, start)
+	rows, err := db.Query(statement)
 
-func getUsers(db *sql.DB, start, count int) ([]user, error)  {
-	return nil ,errors.New("Not implemented")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	users := []user{}
+
+	for rows.Next() {
+		var u user
+		if err := rows.Scan(&u.ID, &u.Name, &u.Age); err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+	}
+
+	return users, nil
 }
